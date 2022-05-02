ee841a316d04f7e9315fefd86b0660e622840ba4
szaccagni <51030579+szaccagni@users.noreply.github.com>
2022-05-01 23:45:51 -0400

    updated carousel logic

--- logics.js
+++ logics.js
@@ -1,19 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    
     // check for carousel
     if (document.querySelector(".carousel")) {
-        // add listener for previous
-        document.querySelector('#carousel-prev').addEventListener('click', previous_img);
-        document.querySelector('#carousel-next').addEventListener('click', next_img);
+        // check for any clicks
+        document.addEventListener('click', event => {
+        const element = event.target;
+            // check if it was on previous
+            if (element.classList.value === 'carousel-prev') {
+                previous_img(element)
+            }
+
+            // check if it was on next
+            if (element.classList.value ==='carousel-next') {
+                next_img(element)
+            }
+        });
     }
 
 });
 
-function previous_img() {
-    const cur_img = document.querySelectorAll('.carousel-active')
+function previous_img(element) {
+    const cur_img = element.parentElement.querySelectorAll('.carousel-active')
     let new_cur_img = cur_img[0].previousElementSibling
-    waiting_imgs = document.querySelectorAll('.carousel-item')
+    waiting_imgs = element.parentElement.querySelectorAll('.carousel-item')
     // check if new img is empty and assign the last waiting img as the next active one
     if (! new_cur_img){
         new_cur_img = waiting_imgs[waiting_imgs.length - 1]
@@ -24,10 +34,10 @@ function previous_img() {
     cur_img[0].classList = 'carousel-item'
 }
 
-function next_img(){
-    const cur_img = document.querySelectorAll('.carousel-active')
+function next_img(element){
+    const cur_img = element.parentElement.querySelectorAll('.carousel-active')
     let new_cur_img = cur_img[0].nextElementSibling
-    waiting_imgs = document.querySelectorAll('.carousel-item')
+    waiting_imgs = element.parentElement.querySelectorAll('.carousel-item')
     // check if new img is empty and assign the first waiting img as the next active one
     if (! new_cur_img) {
         new_cur_img = waiting_imgs[0]
