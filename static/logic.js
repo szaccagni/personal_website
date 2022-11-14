81ae7fd8947441385b4eb20f50a1fc6da477bdbb
szaccagni <51030579+szaccagni@users.noreply.github.com>
2022-11-14 12:35:42 -0500

    fix mobile nav bug

--- static/logic.js
+++ static/logic.js
@@ -6,31 +6,37 @@ document.addEventListener('DOMContentLoaded', function() {
         document.addEventListener('click', event => {
             const element = event.target;
             const bottom = document.querySelector(".header-mobile-bottom");
-            // display links
-            if (bottom.style.display === "none" || bottom.style.display === "") {
-                bottom.style.display = "block";
-                window.setTimeout(function(){
-                    bottom.style.opacity = 1;
-                    bottom.style.transform = 'scale(1)';
-                },0);
-            // hide links
-            } else {
-                bottom.style.opacity = 0;
-                window.setTimeout(function(){
-                    bottom.style.display = 'none';
-                },700);
-            }
             // update hamburger icon
             if (element.classList.value === 'hamburger') {
                 element.classList.value = 'change';
+                toggle(bottom);
             } else if (element.parentElement.classList.value === 'hamburger') {
                 element.parentElement.classList.value = 'change';
+                toggle(bottom);
             } else if (element.classList.value === 'change') {
                 element.classList.value = 'hamburger';
+                toggle(bottom);
             } else if (element.parentElement.classList.value === 'change') {
                 element.parentElement.classList.value = 'hamburger';
+                toggle(bottom);
             }
         });
     }
 
 });
+
+function toggle(bottom) {
+    if (bottom.style.display === "none" || bottom.style.display === "") {
+        bottom.style.display = "block";
+        window.setTimeout(function(){
+            bottom.style.opacity = 1;
+            bottom.style.transform = 'scale(1)';
+        },0);
+    // hide links
+    } else {
+        bottom.style.opacity = 0;
+        window.setTimeout(function(){
+            bottom.style.display = 'none';
+        },700);
+    }
+}
